c325b2c5afcba85d3bce877e905545f97761937f
AgentosUA <oleh.stepaniuk@chatbots.studio>
2024-04-09 23:11:47 +0300

    fix: mimetype

--- src/shared/utils/file.ts
+++ src/shared/utils/file.ts
@@ -1,11 +1,13 @@
 import { readFileSync, existsSync } from 'fs';
 
-const fileToBase64 = (pathToFile: string) => {
+const fileToBase64 = (pathToFile: string, type = 'image') => {
   if (!existsSync(pathToFile)) return '';
 
   const bitmap = readFileSync(pathToFile);
 
-  return Buffer.from(bitmap).toString('base64');
+  const mimetype = type === 'image' ? 'data:image/png;base64' : '';
+
+  return `${mimetype}, ${Buffer.from(bitmap).toString('base64')}`;
 };
 
 export { fileToBase64 };
